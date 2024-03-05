27607a50ea6fca123d45f20f4328b7dc16ada02e
chafinchris97 <chafin.chris.97@gmail.com>
2024-03-05 01:50:24 -0800

    Add config files for deploying into github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://chafinchris97.github.io',
+    base: '/yolo-community-band',
+});
